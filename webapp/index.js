530af92939b4e34d99f7e444548dc279f4fd5a0b
Juan Sebastian Otero <ing.sebastianotero@gmail.com>
2019-11-06 19:17:20 -0500

    Update index.js

--- webapp/index.js
+++ webapp/index.js
@@ -5,6 +5,11 @@ const app = new Koa();
 const router = new Router();
 const sum = require('./sum');
 
+router.get('/', (ctx, next) => {
+  const result = "Hello World";
+  return ctx.body = { result };
+});
+
 router.get('/add/:a/:b', (ctx, next) => {
   const result = sum(parseFloat(ctx.params.a), parseFloat(ctx.params.b));
   return ctx.body = { result };
